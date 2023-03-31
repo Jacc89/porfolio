013907e77eadc6d0979e55ed647aa3cab0fb99a0
jacc89 <laflacalex9@gmail.com>
2023-03-30 21:32:15 -0500

    migrate css a scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { 
-    path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilModule )
-  },
-  { 
-    path: 'proyectos',
-    loadChildren: () => import('./proyectos/proyectos.module').then( m => m.ProyectosModule )
-  },
+ 
 ];
 
 @NgModule({
